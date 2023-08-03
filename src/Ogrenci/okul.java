674c11ab4cb1e85dc57ebcf8ea58a9a6e98b4c42
nazlicansever <n.cansever1@hotmail.com>
2023-08-03 09:44:38 -0700

    ilk taskimi bitirdim

---
+++ src/Ogrenci/okul.java
@@ -0,0 +1,11 @@
+package Ogrenci;
+
+public class okul {
+    public static void main(String[] args) {
+       String ad;
+       String soyad;
+
+
+
+    }
+}
---
+++ src/Ogrenci/ornek.java
@@ -0,0 +1,7 @@
+package Ogrenci;
+
+public class ornek {
+    public static void main(String[] args) {
+
+    }
+}
